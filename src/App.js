eaa0041e617d482ef28a17fdfd10b75e4a1a122f
priyansh gupta <priyansh19csu225@github.com>
2021-10-24 10:04:41 +0530

    React component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render(){
   return (
-    <div >
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">HEllo Priyansh</NavbarBrand>
-          
+
         </div>
-      </Navbar>
-      <Menu></Menu>
+      </Navbar><Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
