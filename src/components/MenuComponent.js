0e6eeac8cc9fe548983f87383c4047684d39c370
priyansh gupta <priyansh19csu225@github.com>
2021-10-24 15:02:28 +0530

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,42 +1,65 @@
-import React , {Component} from "react";
-import { Media } from "reactstrap";
-class Menu extends Component{
-constructor(props){
-    super(props);
-    this.state = {
-       
-    };
-}
-    render() {
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle  } from "reactstrap";
+import DishDetail from './DishDetailComponent';
 
+class Menu extends Component{
+
+    constructor(props){
+        super(props);
+
+        // stores iproperties of this component
+        this.state = {
+            selectedDish: null
+        };
+
+        console.log('Menu component constructed');
+        
+    }
+
+    onDishSelect(dish){
+
+        this.setState({
+            selectedDish: dish
+        });
+
+    }
+
+
+
+    render(){
+        console.log('renders menu component');
+        
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div key = {dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt = {dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>    
-                    </Media>
+                <div key={ dish.id } className="col-12 col-md-5 m-1">    
+                    <Card onClick={ () => this.onDishSelect( dish ) } >
+
+                        <CardImg width="100%" src={ dish.image } alt={ dish.name } />
+                        <CardImgOverlay>
+                            <CardTitle> { dish.name }</CardTitle>
+                        </CardImgOverlay>
+                    </Card>                
                 </div>
             );
         });
 
-        return (
+        return(
             <div className="container">
                 <div className="row">
-                    <Media list style={{listStyle: 'none'}}>
-                        {menu}
-                    </Media>
+                    { menu }
                 </div>
+
+                <DishDetail dish={this.state.selectedDish} />
+                
+
             </div>
         );
+    }
 
-    };
-
-
+    componentDidMount(){
+        console.log('Menu component componentDidMounbt is invoked');
+        
+    }
 }
+
 export default Menu;
